6b61eaec57ff5985e2295e9028a0353f22e2736d
Kristina Morozova <christina.vadi@gmail.com>
2023-07-03 20:46:39 +0600

    feat: add highlight hover effect on html viewer elements

--- rss-css-selectors/src/components/editor.ts
+++ rss-css-selectors/src/components/editor.ts
@@ -3,6 +3,7 @@ class Editor {
 
   constructor(boardMarkup: string) {
     this.boardMarkup = boardMarkup;
+    this.addMouseEventOnMarkup();
   }
 
   public addBoardMarkup():void {
@@ -10,6 +11,25 @@ class Editor {
     const newMarkup = this.getMarkup();
     Editor.clearEditor();
     markup.append(newMarkup);
+  }
+
+  public addMouseEventOnMarkup(): void {
+    const editorMarkup = document.querySelector('.editor__markup') as HTMLDivElement;
+
+    editorMarkup.addEventListener('mouseover', (event: Event): void => {
+      const target = event.target as HTMLDivElement;
+      if (target.classList.length === 0 && target.tagName.toLocaleLowerCase() === 'div' ) {
+        target.classList.add('highlight');
+      }
+    });
+
+    editorMarkup.addEventListener('mouseout', (event: Event): void => {
+      const target = event.target as HTMLDivElement;
+      if (target.tagName.toLocaleLowerCase() === 'div' && target.classList.contains('highlight')) {
+        target.classList.remove('highlight');
+      }
+    });
+
   }
   
   static clearEditor():void {
@@ -31,19 +51,6 @@ class Editor {
   private wrapTagsInDiv(htmlMarkupElement: HTMLElement): HTMLElement | undefined {
     const hasChildren: boolean = (htmlMarkupElement.children.length > 0) ? true : false;
     const wrapper = document.createElement('div') as HTMLElement;
-    const appendElement = (node: Element, element: HTMLElement): void => {
-      if (node.classList.length > 0) {
-        const openTag = `<${node.tagName.toLocaleLowerCase()} class="${node.classList}">`;
-        element.append(openTag);
-      } else {
-        element.append(`<${node.tagName.toLocaleLowerCase()}>`);
-      }
-      const innerElement: HTMLElement | undefined = this.wrapTagsInDiv(node as HTMLElement);
-      if (innerElement !== undefined) {
-        element.append(innerElement);
-      }
-      element.append(`</${node.tagName.toLocaleLowerCase()}>`);
-    };
 
     if (hasChildren) {
       const markupElementChildren: HTMLCollection = htmlMarkupElement.children;
@@ -51,11 +58,11 @@ class Editor {
       for (const node of markupElementChildren) {
         if (markupElementChildren.length > 1) {
           const newElement = document.createElement('div') as HTMLElement;
-          appendElement(node, newElement);
+          this.appendElement(node, newElement);
           wrapper.append(newElement);
         }
         if (markupElementChildren.length === 1) {
-          appendElement(node, wrapper);
+          this. appendElement(node, wrapper);
         }
       }
       
@@ -64,6 +71,20 @@ class Editor {
     }
 
     return wrapper;
+  }
+
+  private appendElement(node: Element, element: HTMLElement): void {
+    if (node.classList.length > 0) {
+      const openTag = `<${node.tagName.toLocaleLowerCase()} class="${node.classList}">`;
+      element.append(openTag);
+    } else {
+      element.append(`<${node.tagName.toLocaleLowerCase()}>`);
+    }
+    const innerElement: HTMLElement | undefined = this.wrapTagsInDiv(node as HTMLElement);
+    if (innerElement !== undefined) {
+      element.append(innerElement);
+    }
+    element.append(`</${node.tagName.toLocaleLowerCase()}>`);
   }
 }
 
